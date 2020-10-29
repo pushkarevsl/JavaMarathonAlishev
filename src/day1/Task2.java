1154f7903d5c559c732b727b3d231a7b741dedf7
puskarev <pqi2gb@mail.ru>
2020-10-29 23:19:16 +0300

    Init commit

---
+++ src/day1/Task2.java
@@ -0,0 +1,14 @@
+package day1;
+
+/**
+ Вывести на экран слово "JAVA", в строку, чтобы оно повторилось 10 раз, используя цикл for
+ */
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        for(int i=0; i<10; i++){
+            System.out.print("JAVA ");
+        }
+    }
+}
