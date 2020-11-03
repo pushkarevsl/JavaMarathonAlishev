f1285e61d993fec4eb017ec6bd42444452cb2cc9
puskarev <pqi2gb@mail.ru>
2020-11-04 00:09:17 +0300

    day 12. List

---
+++ src/day12/task1/Task2.java
@@ -0,0 +1,22 @@
+package day12.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ 2. Создать новый список, заполнить его четными числами от 0 до 30 и от 300 до 350. Вывести список.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+
+        for(int i=1; i<=30; i++){
+            integerList.add(i);
+        }
+        for(int i=300; i<=350; i++){
+            integerList.add(i);
+        }
+
+        System.out.println(integerList.toString());
+    }
+}
