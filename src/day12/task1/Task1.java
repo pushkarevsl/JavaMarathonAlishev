8bfb490ae253d9d709e243932ef5c6f6a600d197
puskarev <pqi2gb@mail.ru>
2020-11-03 22:58:08 +0300

    day 12. List

---
+++ src/day12/task1/Task1.java
@@ -0,0 +1,28 @@
+package day12.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ 1. Создать список строк, добавить в него 5 марок автомобилей, вывести список в консоль.
+ Добавить в середину еще 1 автомобиль, удалить самый первый автомобиль из списка.
+ Распечатать список.
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        List<Auto> autos = new ArrayList<Auto>(){{
+            add(new Auto("BMW"));
+            add(new Auto("audi"));
+            add(new Auto("Mercedes"));
+            add(new Auto("Lada"));
+        }};
+
+        autos.add(2, new Auto("Niva"));
+
+        autos.remove(3);
+
+        System.out.println(autos);
+    }
+}
